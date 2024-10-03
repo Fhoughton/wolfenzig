0a345ce1760a538279234efae35d80cb768fe165
Felix Houghton <fdphoughton@protonmail.com>
2024-10-03 12:25:33 +0100

    Add basic map drawing code

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,17 @@ const rl = @import("raylib");
 
 var playerPos = rl.Vector2{ .x = 100, .y = 100 };
 
+const map = [8][8]i32{
+    [_]i32{1,1,1,1,1,1,1,1},
+    [_]i32{1,0,0,0,0,0,0,1},
+    [_]i32{1,0,0,0,1,1,1,1},
+    [_]i32{1,0,0,0,1,0,0,1},
+    [_]i32{1,0,0,0,1,0,1,1},
+    [_]i32{1,0,1,0,1,0,0,1},
+    [_]i32{1,0,0,0,0,0,0,1},
+    [_]i32{1,1,1,1,1,1,1,1},
+};
+
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -30,12 +41,22 @@ pub fn main() anyerror!void {
             playerPos.x += 2;
         }
 
-        // Draw
+        // Begin Drawing
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
 
+        // Draw Map
+        for (0..8) |x| {
+            for (0..8) |y| {
+                if (map[x][y] == 1) {
+                    rl.drawRectangle(@as(i32, @intCast(x*32)), @as(i32, @intCast(y*32)), 32, 32, rl.Color.blue);
+                }
+            }
+        }
+
+        // Draw Player
         rl.drawCircleV(playerPos, 16.0, rl.Color.red);
     }
 }
